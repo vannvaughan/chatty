54be8ab904418c59b9775a36546bf4a1a9f4f772
vannvaughan <vann.vaughan@yahoo.com>
2017-06-13 14:59:26 -0500

    posted messages

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+app.get('/messages', function(req, res, next){
+    res.status(200).json({messages: messages});
+});
+
+app.post('/messages', function(req, res, next){
+    console.log(req.body);
+});
+
+app.listen(3000, function(){
+    console.log('This Guy!')
+});
+
+var messages = [];
 
