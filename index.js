be8aac1b7c45caa62b402aaf090a8913993ea6f4
vannvaughan <vann.vaughan@yahoo.com>
2017-06-13 15:51:31 -0500

    completed all steps

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var messages = [];
 
 app.use(express.static('assets'));
 app.use(bodyParser.json());
@@ -10,12 +11,14 @@ app.get('/messages', function(req, res, next){
 });
 
 app.post('/messages', function(req, res, next){
-    console.log(req.body);
+    messages.push({ message: req.body.message, time: new Date() });
+    res.status(200).json({ messages: messages });
 });
 
 app.listen(3000, function(){
     console.log('This Guy!')
 });
 
-var messages = [];
+
+
 
